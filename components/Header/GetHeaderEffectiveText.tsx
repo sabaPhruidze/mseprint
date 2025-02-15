f5033740a38d32d697e6e998f1c18ef7b29d432d
Saba Phruidze <sabaphruidze01@gmail.com>
2025-02-15 14:43:25 +0400

    corrected the effective text design

--- components/Header/GetHeaderEffectiveText.tsx
+++ components/Header/GetHeaderEffectiveText.tsx
@@ -6,14 +6,17 @@ const GetHeaderEffectiveText = ({
 }: {
   effectiveTextData: headerEffectiveTextTypes[];
 }) => {
-  console.log(effectiveTextData);
   return (
-    <div className="text-black font-bold text-2xl flex items-center justify-center skew-x-[-15deg] text-center">
-      {effectiveTextData.map((item) => (
-        <span key={item.id} className="px-2 skew-x-[15deg]">
-          {item.text}
-          <span className="px-2">{item.circle}</span>
-        </span>
+    <div className="text-black font-bold text-2xl flex items-center justify-center text-center">
+      {effectiveTextData.map((item, index) => (
+        <React.Fragment key={item.id}>
+          {/* Skew only the text */}
+          <span className="skew-x-[-15deg] italic">{item.text}</span>
+          {/* Show the circle except for the last item */}
+          {index < effectiveTextData.length - 1 && (
+            <span className="px-2 not-italic">{item.circle}</span>
+          )}
+        </React.Fragment>
       ))}
     </div>
   );
