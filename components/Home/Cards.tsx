ffb05ef8e3d8fc86947e5b63eb1f24b42d857ed6
Saba Phruidze <sabaphruidze01@gmail.com>
2025-02-22 19:16:10 +0400

    improved image quality

--- components/Home/Cards.tsx
+++ components/Home/Cards.tsx
@@ -14,13 +14,16 @@ const Cards: React.FC<CardsProps> = ({ cardsData, homeSpecialities }) => {
   }
 
   return (
-    <div className="flex flex-col lg:flex-row gap-6 items-center screen-size-18:max-w-[1850px] mx-auto">
-      {/* Specialities Section - Centered */}
+    <section
+      aria-labelledby="cards-section"
+      className="flex flex-col lg:flex-row gap-6 items-center screen-size-18:max-w-[1850px] mx-auto"
+    >
+      <h2 id="cards-section" className="sr-only">
+        Featured Products & Services
+      </h2>
       <div className="lg:w-1/3 flex justify-center">
         <Specialities specialities={homeSpecialities} />
       </div>
-
-      {/* Cards Section - Centered when <1024px, Normal Otherwise */}
       <div className="lg:w-2/3 flex md:justify-start justify-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 screen-size-15:grid-cols-3 xl:grid-cols-2 gap-6 p-4">
           {cardsData.map((card) => (
@@ -33,7 +36,7 @@ const Cards: React.FC<CardsProps> = ({ cardsData, homeSpecialities }) => {
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
