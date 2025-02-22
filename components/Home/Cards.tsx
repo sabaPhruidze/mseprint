1de1ba3efa0b4ec43fbcc3f90d54fb32658fcc64
Saba Phruidze <sabaphruidze01@gmail.com>
2025-02-22 17:33:55 +0400

    Card is almost finished

--- components/Home/Cards.tsx
+++ components/Home/Cards.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { SEOImageProps } from "../../types/commonTypes";
 import Link from "next/link";
-import SEOImage from "../../components/common/SEOImage"; // Ensure you have this component
+import SEOImage from "../../components/common/SEOImage";
 
 interface CardsProps {
   cardsData: SEOImageProps[];
@@ -13,13 +13,13 @@ const Cards: React.FC<CardsProps> = ({ cardsData }) => {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4 cursor-pointer">
       {cardsData.map((card) => (
         <div
           key={card.alt}
-          className="bg-gray-800 text-white rounded-lg shadow-lg overflow-hidden"
+          className="bg-white text-black rounded-lg shadow-lg overflow-hidden max-w-[400px] mx-auto border border-mediumGray transition-transform transform hover:scale-105"
         >
-          <div className="relative w-full h-48">
+          <div className="relative w-full h-[250px]">
             <Link href={card.path || "/"} passHref>
               <SEOImage
                 src={`/images/${card.src}`}
@@ -28,7 +28,7 @@ const Cards: React.FC<CardsProps> = ({ cardsData }) => {
                 geoData={card.geoData}
                 priority={card.priority}
                 loading={card.priority ? undefined : "lazy"}
-                sizes={card.sizes}
+                sizes="400px auto"
                 fill
               />
             </Link>
@@ -36,7 +36,7 @@ const Cards: React.FC<CardsProps> = ({ cardsData }) => {
 
           <div className="p-4">
             <h2 className="text-lg font-semibold">{card.alt}</h2>
-            <p className="text-gray-300">{card.description}</p>
+            <p className="text-gray-700">{card.description}</p>
           </div>
         </div>
       ))}
