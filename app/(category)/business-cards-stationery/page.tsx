72861dec19e8695895dbe7b81caa9e0fef4fa579
Saba Phruidze <sabaphruidze01@gmail.com>
2025-03-11 22:28:49 +0400

    removed unnided and added needed 3 page

--- app/(category)/business-cards-stationery/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BusinessCardsStationery = () => {
-  return <div>BusinessCardsStationery</div>;
-};
-
-export default BusinessCardsStationery;
---
+++ app/(category)/custom-business-card-printing/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomBusinessCardPrinting = () => {
+  return <div>CustomBusinessCardPrinting</div>;
+};
+
+export default CustomBusinessCardPrinting;
