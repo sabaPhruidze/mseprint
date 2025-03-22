20903875df322502f641981b82c4264a5db585a4
Saba Phruidze <sabaphruidze01@gmail.com>
2025-03-22 21:56:19 +0400

    partly added

--- components/Home/Cards.tsx
+++ components/Home/Cards.tsx
@@ -13,17 +13,19 @@ const Cards: React.FC<homeSpecialityCardsTypes> = ({
   }
 
   return (
-    <section>
+    <section className="mx-auto flex flex-col items-center screen-size-18:max-w-[1850px]">
       <div
         aria-labelledby="cards-section"
-        className="flex flex-col xl:flex-row gap-6 items-center screen-size-18:max-w-[1850px] mx-auto"
+        className="flex flex-col xl:flex-row items-center gap-6 w-full"
       >
         <h2 id="cards-section" className="sr-only">
           Featured Products & Services
         </h2>
+
         <div className="xl:w-1/3 flex justify-center">
           <Specialities specialities={homeSpecialities} />
         </div>
+
         <div className="xl:w-2/3 flex md:justify-start justify-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 screen-size-15:grid-cols-3 xl:grid-cols-2 gap-6 p-4">
             {cardsData.map((card, index) => (
@@ -36,8 +38,8 @@ const Cards: React.FC<homeSpecialityCardsTypes> = ({
             ))}
           </div>
         </div>
-        <div className="clear-both" />
       </div>
+
       <BelowAdditionalSpecialities specialities={homeSpecialities} />
     </section>
   );
