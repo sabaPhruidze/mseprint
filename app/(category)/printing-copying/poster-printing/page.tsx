cb054f385688ab8dd688b286c3b6b513b38bbe22
Saba Phruidze <sabaphruidze01@gmail.com>
2025-04-06 23:42:48 +0400

    schange

--- app/(category)/printing-copying/poster-printing/page.tsx
+++ app/(category)/printing-copying/poster-printing/page.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     ],
   },
 };
-
+//change
 const PosterPrinting = async () => {
   const data = await getCategoryPagesData("/printing-copying/poster-printing");
   const pageData = data.PosterPrintingPageData?.[0];
