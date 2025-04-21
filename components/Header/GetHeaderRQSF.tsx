2272166f36073cca998a37cd289332c85ef78b44
Saba Phruidze <sabaphruidze01@gmail.com>
2025-04-21 10:53:26 +0400

    regulated the getHeaderRQSF so it will work well and be responsive for 360px as well on phones, more minimazed phone hope not be seen

--- components/Header/GetHeaderRQSF.tsx
+++ components/Header/GetHeaderRQSF.tsx
@@ -10,7 +10,7 @@ const GetHeaderRQSF: FC<GetRequestQuoteSendAFileProps> = ({ rqsafData }) => {
   return (
     <nav
       className="
-        w-full h-16 bg-purple flex items-center screen-size-13:justify-end justify-center screen-size-5:px-4 px-2
+        w-full screen-size-5:h-[60px] h-[100px] bg-purple flex items-center screen-size-13:justify-end justify-center screen-size-5:px-4 screen-size-4:px-2 px-0
       "
       role="navigation"
       aria-label="Request a Quote and Order Navigation"
@@ -27,7 +27,8 @@ const GetHeaderRQSF: FC<GetRequestQuoteSendAFileProps> = ({ rqsafData }) => {
             rounded-lg
             px-4
             py-2
-            mx-2
+            screen-size-4:mx-2
+            mx-0.5
             no-underline
             hover:bg-white
             hover:text-black
@@ -53,7 +54,8 @@ const GetHeaderRQSF: FC<GetRequestQuoteSendAFileProps> = ({ rqsafData }) => {
           rounded-lg
           px-4
           py-2
-          mx-2
+          screen-size-4:mx-2
+          mx-0.5
           no-underline
           hover:bg-white
           hover:text-black
