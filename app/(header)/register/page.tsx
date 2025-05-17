71a9575691e6b9cd4c49b26e349164f33e4a86d0
Saba Phruidze <sabaphruidze01@gmail.com>
2025-05-17 19:22:45 +0400

    I removed clerk because did not understand it and now try any other thing

--- app/(header)/register/page.tsx
+++ app/(header)/register/page.tsx
@@ -1,5 +1,7 @@
-const register = () => {
-  return <div>register</div>;
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
 };
 
-export default register;
+export default page;
