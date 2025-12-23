f604e92b8f700d94994155a5b79a821aadc73f08
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-24 00:03:31 +0400

    this is the cleaner version

--- components/common/PageStructure.tsx
+++ components/common/PageStructure.tsx
@@ -1,56 +1,45 @@
 import { PageStructureTypes } from "types/commonTypes";
 import type { BreadcrumbItem } from "./page-structure/Breadcrumbs";
+import type { LocationTokens } from "./page-structure/utils";
+
+import HeroSection from "./page-structure/HeroSection";
 import OnThisPageNav from "./page-structure/OnThisPageNav";
 import WhyChooseSection from "./page-structure/WhyChooseSection";
 import ServicesSection from "./page-structure/ServicesSection";
 import OfferingsSection from "./page-structure/OfferingsSection";
-import FaqSection from "./page-structure/FaqSection";
-import GetStartedSection from "./page-structure/GetStartedSection";
 import AdvancedFeaturesSection from "./page-structure/AdvancedFeaturesSection";
 import HowToGetStartedSection from "./page-structure/HowToGetStartedSection";
 import WhyTrustUsSection from "./page-structure/WhyTrustUsSection";
-import HeroSection from "./page-structure/HeroSection";
-import { LocationTokens } from "./page-structure/utils";
-
-interface PageStructureProps {
-  pageData: PageStructureTypes;
-  tokens?: LocationTokens;
-  breadcrumbs?: BreadcrumbItem[];
-}
+import FaqSection from "./page-structure/FaqSection";
+import GetStartedSection from "./page-structure/GetStartedSection";
 
 export default function PageStructure({
   pageData,
   tokens,
   breadcrumbs,
-}: PageStructureProps) {
+}: {
+  pageData: PageStructureTypes;
+  tokens?: LocationTokens;
+  breadcrumbs?: BreadcrumbItem[];
+}) {
   return (
-    <>
-      <main className="text-lg" role="main">
-        <HeroSection
-          pageData={pageData}
-          tokens={tokens}
-          breadcrumbs={breadcrumbs}
-        />
-        <OnThisPageNav pageData={pageData} />
-        <div className="container mx-auto p-8 max-w-[1500px] text-left">
-          <div className="container py-8 max-w-[1500px]">
-            <WhyChooseSection pageData={pageData} />
-            {/* SERVICES SECTION - No collapse */}
-            <ServicesSection pageData={pageData} tokens={tokens} />
-            {/* OFFERINGS SECTION - No collapse */}
-            <OfferingsSection pageData={pageData} tokens={tokens} />
-          </div>
-          <AdvancedFeaturesSection pageData={pageData} />
-
-          <HowToGetStartedSection pageData={pageData} />
-          {/* ── Why Trust Us ── */}
-          <WhyTrustUsSection pageData={pageData} />
-          {/* FAQs */}
-          <FaqSection pageData={pageData} tokens={tokens} />
-          {/* FAQ */}
-          <GetStartedSection pageData={pageData} />
-        </div>
-      </main>
-    </>
+    <main className="text-lg" role="main">
+      <HeroSection
+        pageData={pageData}
+        tokens={tokens}
+        breadcrumbs={breadcrumbs}
+      />
+      <OnThisPageNav pageData={pageData} />
+      <div className="container mx-auto p-8 max-w-[1500px] text-left">
+        <WhyChooseSection pageData={pageData} />
+        <ServicesSection pageData={pageData} tokens={tokens} />
+        <OfferingsSection pageData={pageData} tokens={tokens} />
+        <AdvancedFeaturesSection pageData={pageData} />
+        <HowToGetStartedSection pageData={pageData} />
+        <WhyTrustUsSection pageData={pageData} />
+        <FaqSection pageData={pageData} tokens={tokens} />
+        <GetStartedSection pageData={pageData} />
+      </div>
+    </main>
   );
 }
